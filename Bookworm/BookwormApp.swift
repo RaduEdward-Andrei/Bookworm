dc888c48d03c620bef9abd93326bbdbd3080fb60
Radu Edward-Andrei <raduedwardandrei@gmail.com>
2026-03-26 13:29:22 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Radu Edward-Andrei on 26.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
